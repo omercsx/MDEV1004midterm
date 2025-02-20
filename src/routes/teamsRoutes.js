29e8c001f5d25c3120d7db5548c2f6030a1af257
omercsx <omercagris@gmail.com>
2025-02-20 11:24:03 -0500

    Add teams routes with CRUD operations

--- src/routes/teamsRoutes.js
+++ src/routes/teamsRoutes.js
@@ -2,5 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // controllers import
+const { insertTeams, getAllTeams, getTeamById, getTeamsByCity } = require("../controller/teamsController");
 
+// routes 
+router.post("/teams/insert", insertTeams);
+router.get("/teams", getAllTeams);
+router.get("/teams/:id", getTeamById);
+router.get("/teams/city/:city", getTeamsByCity);
 
+module.exports = router;
